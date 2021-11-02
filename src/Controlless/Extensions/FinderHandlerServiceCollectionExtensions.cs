400bbfdc316b7536fae38a6b1f96a7dd99a063ef
Connell <connellsharp@gmail.com>
2021-11-02 22:00:18 +0000

    Scan assembly for types that can be registered as binders

--- src/Controlless/Extensions/FinderHandlerServiceCollectionExtensions.cs
+++ src/Controlless/Extensions/FinderHandlerServiceCollectionExtensions.cs
@@ -1,12 +1,13 @@
 using System;
+using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Controlless
 {
     public static class FinderHandlerServiceCollectionExtensions
     {
-        public static void AddControlless(
-           this IServiceCollection services)
+        public static void AddControllessCore(this IServiceCollection services)
         {
             if (services == null)
                 throw new ArgumentNullException(nameof(services));
@@ -14,6 +15,38 @@ namespace Controlless
             services.AddSingleton(typeof(RequestHandlerWeakAdapter<>));
             services.AddSingleton(typeof(ResponseWriterWeakAdapter<>));
             services.AddSingleton(typeof(IResponseWriter<>), typeof(JsonResponseWriter<>));
+        }
+
+        public static void AddRequestBinders(this IServiceCollection services, Type type)
+            => services.AddRequestBinders(type.Assembly);
+
+        public static void AddRequestBinders(this IServiceCollection services, Assembly assembly)
+        {
+            services.AddControllessCore();
+
+            var binderTypes = assembly.GetExportedTypes()
+                .Where(t => typeof(IRequestBinder).IsAssignableFrom(t));
+
+            foreach(var binderType in binderTypes)
+            {
+                services.AddSingleton(typeof(IRequestBinder), binderType);
+            }
+        }
+
+        public static void AddRequestModelBinders(this IServiceCollection services, Type type)
+            => services.AddRequestModelBinders(type.Assembly);
+
+        public static void AddRequestModelBinders(this IServiceCollection services, Assembly assembly)
+        {
+            services.AddControllessCore();
+
+            var modelTypes = assembly.GetExportedTypes()
+                .Where(t => t.GetCustomAttribute<RouteAttribute>() != null);
+
+            foreach(var modelType in modelTypes)
+            {
+                services.AddSingleton(typeof(IRequestBinder), typeof(ModelRequestBinder<>).MakeGenericType(modelType));
+            }
         }
     }
 }
