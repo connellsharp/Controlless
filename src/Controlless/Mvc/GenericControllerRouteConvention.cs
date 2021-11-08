404d8b7a7538a47e03f73bce6260432467c8986f
Connell <connellsharp@gmail.com>
2021-11-08 19:26:37 +0000

    Add all action filters from the request class onto the action

--- src/Controlless/Mvc/GenericControllerRouteConvention.cs
+++ src/Controlless/Mvc/GenericControllerRouteConvention.cs
@@ -1,6 +1,8 @@
+using System.Linq;
 using System.Reflection;
 using Microsoft.AspNetCore.Mvc.ActionConstraints;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Controlless.Binding
 {
@@ -11,19 +13,37 @@ namespace Controlless.Binding
             if (controller.ControllerType.IsGenericType
                 && controller.ControllerType.GetGenericTypeDefinition() == typeof(ControllerlessBaseController<>))
             {
-                var genericType = controller.ControllerType.GenericTypeArguments[0];
-                var routeAttribute = genericType.GetCustomAttribute<RouteAttribute>();
-    
-                if (routeAttribute != null)
+                AddRoutingAttributes(controller);
+                AddFilterAttributes(controller);
+            }
+        }
+
+        private static void AddRoutingAttributes(ControllerModel controller)
+        {
+            var genericType = controller.ControllerType.GenericTypeArguments[0];
+            var routeAttribute = genericType.GetCustomAttribute<RouteAttribute>();
+
+            if (routeAttribute != null)
+            {
+                controller.Selectors.Add(new SelectorModel
                 {
-                    controller.Selectors.Add(new SelectorModel
+                    AttributeRouteModel = new AttributeRouteModel(new Microsoft.AspNetCore.Mvc.RouteAttribute(routeAttribute.RoutePattern)),
+                    ActionConstraints =
                     {
-                        AttributeRouteModel = new AttributeRouteModel(new Microsoft.AspNetCore.Mvc.RouteAttribute(routeAttribute.RoutePattern)),
-                        ActionConstraints = {
-                            new HttpMethodActionConstraint(new[] { routeAttribute.Method })
-                        }
-                    });
-                }
+                        new HttpMethodActionConstraint(new[] { routeAttribute.Method })
+                    }
+                });
+            }
+        }
+
+        private static void AddFilterAttributes(ControllerModel controller)
+        {
+            var genericType = controller.ControllerType.GenericTypeArguments[0];
+            var filterAttributes = genericType.GetCustomAttributes().OfType<IFilterMetadata>();
+
+            foreach(var filterAttribute in filterAttributes)
+            {
+                controller.Filters.Add(filterAttribute);
             }
         }
     }
